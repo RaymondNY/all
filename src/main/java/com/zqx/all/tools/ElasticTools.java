cd693fd47b92d21e353155a1f1a29226912bfdd0
zhangqixuan <zhangqixuan@qq.com>
2023-07-13 17:01:09 +0800

    1

--- src/main/java/com/zqx/all/tools/ElasticTools.java
+++ src/main/java/com/zqx/all/tools/ElasticTools.java
@@ -1,9 +1,12 @@
 package com.zqx.all.tools;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch.indices.CreateIndexResponse;
+import com.alibaba.fastjson2.JSONObject;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 
 @Component
@@ -12,5 +15,10 @@ public class ElasticTools {
     @Resource
     private ElasticsearchClient elasticsearchClient;
 
-
+    public void createIndex(JSONObject jsonObject) throws IOException {
+        String index = jsonObject.getString("index");
+        CreateIndexResponse response = elasticsearchClient.indices().create(i -> i
+                .index(index));
+        jsonObject.getString("");
+    }
 }
